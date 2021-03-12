b445df085e2dc8dcc2357f3c3147325a3064f4fa
bhfreeman <brett.h.freeman@gmail.com>
2021-03-11 20:51:17 -0700

    fixed forecast function, made code more concise

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,13 +4,13 @@ var forecastContainer = document.querySelector("#forecast-container");
 var searchBtn = document.querySelector("#search-btn");
 var cityText = document.querySelector("#city-text");
 var stateText = document.querySelector("#state-text");
-var temperature;
-var humidity;
-var windSpeed;
-var uvIndex;
-var headName;
-var date;
-var weatherIcon;
+// var temperature;
+// var humidity;
+// var windSpeed;
+// var uvIndex;
+// var headName;
+// var date;
+// var weatherIcon;
 
 function getLocation(city) {
   var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
@@ -27,27 +27,18 @@ function getLocation(city) {
         .then((data) => data.json())
         .then(function (data) {
           console.log(data);
-          temperature = data.current.temp;
-          humidity = data.current.humidity;
-          windSpeed = data.current.wind_speed;
-          uvIndex = data.current.uvi;
-          weatherIcon = data.current.weather[0].icon;
-          date = data.current.dt;
-          // console.log(data);
-          currentWeatherCard();
+          currentWeatherCard(data);
+          forecastContainer.innerHTML = "";
           for (let i = 0; i < 4; i++) {
             item = data.daily[i];
-            humidity = item.humidity;
-            temperature = item.temp.day;
-            date = item.dt;
-            weatherIcon = item.weather[0].icon;
-            forecastCard();
+            console.log(item);
+            forecastCard(item);
           }
         });
     });
 }
 
-function currentWeatherCard() {
+function currentWeatherCard(item) {
   cardContainer.innerHTML = "";
   var newCard = document.createElement("div");
   var headNameH1 = document.createElement("h1");
@@ -59,31 +50,27 @@ function currentWeatherCard() {
   var iconImg = document.createElement("img");
   iconImg.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+    `http://openweathermap.org/img/wn/${item.current.weather[0].icon}@2x.png`
   );
-  headNameH1.innerText = `${headName} ${date}`;
+  headNameH1.innerText = `${headName} ${item.current.dt}`;
   headNameH1.append(iconImg);
-  tempLi.innerText = `Temperature: ${temperature}`;
-  humidityLi.innerText = `Humidity: ${humidity}`;
-  windLi.innerText = `Wind Speed: ${windSpeed}`;
-  uvLi.innerText = `UV Index: ${uvIndex}`;
+  tempLi.innerText = `Temperature: ${item.current.temp}`;
+  humidityLi.innerText = `Humidity: ${item.current.humidity}`;
+  windLi.innerText = `Wind Speed: ${item.current.wind_speed}`;
+  uvLi.innerText = `UV Index: ${item.current.uvi}`;
   ul.classList = "list-group";
   tempLi.classList = "list-group-item";
   humidityLi.classList = "list-group-item";
   windLi.classList = "list-group-item";
   uvLi.classList = "list-group-item";
 
-  ul.append(tempLi);
-  ul.append(humidityLi);
-  ul.append(windLi);
-  ul.append(uvLi);
+  ul.append(tempLi, humidityLi, windLi, uvLi);
   newCard.append(headNameH1);
   newCard.append(ul);
   cardContainer.append(newCard);
 }
 
-function forecastCard() {
-  forecastContainer.innerHTML = "";
+function forecastCard(data) {
   var newCard = document.createElement("div");
   var headH1 = document.createElement("h1");
   var ul = document.createElement("ul");
@@ -92,16 +79,14 @@ function forecastCard() {
   var iconImg = document.createElement("img");
   iconImg.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+    `http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`
   );
-  headH1.innerText = `${date}`;
-  tempLi.innerText = `Temperature: ${temperature}`;
-  humidityLi.innerText = `Humidity: ${humidity}`;
-  newCard.classList = "col-2"
+  headH1.innerText = `${data.dt}`;
+  tempLi.innerText = `Temperature: ${data.temp.day}`;
+  humidityLi.innerText = `Humidity: ${data.humidity}`;
+  newCard.classList = "col-2";
   newCard.append(headH1);
-  ul.append(iconImg);
-  ul.append(tempLi);
-  ul.append(humidityLi);
+  ul.append(iconImg, tempLi, humidityLi);
   newCard.append(ul);
   forecastContainer.append(newCard);
 }
